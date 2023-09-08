9cb0b0dce59f197b788deeab950ddf94998280ea
adil.farooque <adl.frq007@gmail.com>
2023-09-09 02:44:11 +0530

    created hotel-service crud operation

--- hotel-service/src/main/java/com/microservices/exception/HotelException.java
+++ hotel-service/src/main/java/com/microservices/exception/HotelException.java
@@ -1,2 +1,7 @@
-package com.microservices.exception;public class HotelException {
+package com.microservices.exception;
+
+public class HotelException extends RuntimeException{
+    public HotelException(String message) {
+        super(message);
+    }
 }
--- hotel-service/src/main/java/com/microservices/service/impl/HotelServiceImpl.java
+++ hotel-service/src/main/java/com/microservices/service/impl/HotelServiceImpl.java
@@ -1,24 +1,41 @@
-package com.microservices.service;
+package com.microservices.service.impl;
 
 import com.microservices.dto.request.HotelRequest;
 import com.microservices.dto.response.HotelResponse;
+import com.microservices.entity.Hotel;
 import com.microservices.exception.HotelException;
+import com.microservices.repository.HotelRepository;
+import com.microservices.service.HotelService;
+import com.microservices.util.Convertor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-public class HotelServiceImpl implements HotelService{
+@Service
+public class HotelServiceImpl implements HotelService {
+    @Autowired
+    HotelRepository hotelRepository;
     @Override
     public HotelResponse saveHotel(HotelRequest request) throws HotelException {
-        return null;
+        Hotel hotel = Convertor.dtoToEntity(request);
+        hotel.setId(UUID.randomUUID().toString());
+        Hotel savedHotel = hotelRepository.save(hotel);
+        return Convertor.entityToDto(savedHotel);
     }
 
     @Override
     public HotelResponse getHotelById(String id) throws HotelException {
-        return null;
+        Optional<Hotel> hotel = hotelRepository.findById(id);
+        hotel.orElseThrow(()->new HotelException("No hotel found for given id "+id));
+        return Convertor.entityToDto(hotel.get());
     }
 
     @Override
     public List<HotelResponse> getAllHotel() throws HotelException {
-        return null;
+        List<Hotel> hotels = hotelRepository.findAll();
+        return Convertor.entityToDto(hotels);
     }
 }
